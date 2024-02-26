25da199f19327f0396c08d07dde0ff1df9e5cef6
Jamal Salim <Jamalsalim.js12@gmail.com>
2024-02-26 23:50:44 +0000

    Initial Commit

---
+++ myblogapp/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      isOpen: opened,
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div v-if="isOpen">
+      <h1>This is opened</h1>
+    </div>
+
+    <div v-else>
+      <h1>This is closed</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
